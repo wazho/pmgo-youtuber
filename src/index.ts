2b98e586d81b7ee60cee9bea35207ced9a939542
Ze-Hao Wang (Salmon) <salmon.zh.tw@gmail.com>
2021-01-19 19:50:57 +0800

    add more info of channel & handle exception

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Node modules.
+import { orderBy } from 'lodash';
 import { mkdirp, writeFile } from 'fs-extra';
 import { google } from 'googleapis';
 import { XmlEntities } from 'html-entities';
@@ -21,8 +22,8 @@ const getVideos = async (channelId: string) => {
     maxResults: 10,
   });
 
-  const videos = response.data.items;
-  const results = videos?.map((video) => ({
+  const videos = response.data.items || [];
+  const results = videos.map((video) => ({
     title: entities.decode(String(video.snippet?.title)),
     url: `https://www.youtube.com/watch?v=${video.snippet?.resourceId?.videoId}`,
     description: String(video.snippet?.description),
@@ -32,22 +33,47 @@ const getVideos = async (channelId: string) => {
   }));
 
   return results;
-}
+};
+
+const getChannels = async (channelIds: string[]) => {
+  const service = google.youtube('v3');
+  const response = await service.channels.list({
+    auth: API_KEY,
+    part: ['snippet', 'statistics'],
+    id: channelIds,
+  });
+
+  const channels = response.data.items || [];
+  const results = await Promise.all(channels.map(async (channel) => ({
+    id: channel.id!,
+    title: String(channel.snippet?.title),
+    thumbnailUrl: String(channel.snippet?.thumbnails?.default?.url),
+    viewCount: parseInt(channel.statistics?.viewCount || '0'),
+    subscriberCount: parseInt(channel.statistics?.subscriberCount || '0'),
+    videos: await getVideos(channel.id!),
+  })));
+
+  return results;
+};
 
 const main = async () => {
   const outputPath = './artifacts';
   await mkdirp(outputPath);
 
-  const channels: any[] = [];
-  for await (const channel of CHANNEL_LIST) {
-    const videos = await getVideos(channel.channelId);
-    channels.push({
-      name: channel.youtuberAlias,
-      videos,
-    });
-  }
+  let results;
 
-  await writeFile(`${outputPath}/channels.json`, JSON.stringify(channels));
+  try {
+    const channelIds = CHANNEL_LIST.map(({ channelId }) => channelId);
+    const channelsRaw = await getChannels(channelIds);
+    const channels = orderBy(channelsRaw, ['subscriberCount'], ['desc']);
+
+    results = channels;
+  } catch (e) {
+    results = [];
+  } finally {
+    await writeFile(`${outputPath}/channels.json`, JSON.stringify(results, null, 2));
+    await writeFile(`${outputPath}/channels.min.json`, JSON.stringify(results));
+  }
 };
 
 main();
